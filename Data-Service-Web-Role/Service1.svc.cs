2db7674fcbacdce0785d901994cac08318596404
Will <Will@DESKTOP-118CJ5O>
2019-03-25 12:45:45 -0700

    added code for querying sql itemDB

--- Data-Service-Web-Role/Service1.svc.cs
+++ Data-Service-Web-Role/Service1.svc.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace Data_Service_Web_Role
 {
@@ -14,12 +15,84 @@ namespace Data_Service_Web_Role
     {
         public Item GetItemById(int itemId)
         {
-            return new Item();
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                //replace these fields with itemDB credentials
+                builder.DataSource = "";//ex: "<server>.database.windows.net";
+                builder.UserID = "";//ex: "<username>";
+                builder.Password = "";//ex: "<password>";
+                builder.InitialCatalog = "";//ex: "<database>";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();//open the connection to the database
+                    StringBuilder sb = new StringBuilder();
+                    //example for how to build query:
+                    //sb.Append("SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName ");
+                    //sb.Append("FROM [SalesLT].[ProductCategory] pc ");
+                    //sb.Append("JOIN [SalesLT].[Product] p ");
+                    //sb.Append("ON pc.productcategoryid = p.productcategoryid;");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return new Item();//fill this item with data from ItemDB
         }
 
         public Item GetItemByCategory(int categoryId)
         {
-            return new Item();
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                //replace these fields with itemDB credentials
+                builder.DataSource = "";//ex: "<server>.database.windows.net";
+                builder.UserID = "";//ex: "<username>";
+                builder.Password = "";//ex: "<password>";
+                builder.InitialCatalog = "";//ex: "<database>";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();//open the connection to the database
+                    StringBuilder sb = new StringBuilder();
+                    //example for how to build query:
+                    //sb.Append("SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName ");
+                    //sb.Append("FROM [SalesLT].[ProductCategory] pc ");
+                    //sb.Append("JOIN [SalesLT].[Product] p ");
+                    //sb.Append("ON pc.productcategoryid = p.productcategoryid;");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return new Item();//fill this item with data from ItemDB
         }
     }
 }
